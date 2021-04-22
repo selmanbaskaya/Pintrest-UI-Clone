0aa63339d3e3e4084d0e2c39509ec25e5728c2b2
Selman <selmanbaskaya1@gmail.com>
2021-04-23 02:39:27 +0300

    user avatar & unlock tools area added

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -35,7 +35,7 @@ i
             class="mt-7"
             flat
             solo-inverted
-			prepend-inner-icon="mdi-magnify"
+            prepend-inner-icon="mdi-magnify"
           />
         </v-flex>
         <v-btn class="mx-1 ml-4">
@@ -101,6 +101,39 @@ i
           </v-list-item-avatar>
         </v-list-item>
       </v-list>
+      <v-card flat>
+        <v-img
+          src="https://assets.website-files.com/5bff8886c3964a992e90d465/5bff98f85ea7faf86504605e_scenes.gif"
+        />
+        <v-layout class="pt-5 px-8">
+          <v-flex text-center>
+            <div class="subtitle-2 blue--text">
+              Unlock Business Tools
+            </div>
+            <div class="caption">
+              Hurry Up! Now you can unlock out new business tools at your
+              Convenience.
+            </div>
+          </v-flex>
+        </v-layout>
+      </v-card>
+      <v-layout align-center>
+        <v-card elevation="5">
+          <v-layout>
+            <v-avatar>
+              <v-img
+                src="https://images.unsplash.com/photo-1610024062303-e355e94c7a8c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mjd8fHByb2ZpbGUlMjBpbWFnZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+              />
+            </v-avatar>
+            <v-flex class="ml-2">
+              <div class="subtitle-2">
+                Selman Baskaya
+              </div>
+              <div class="body-2 greyy--text">Software Engineer</div>
+            </v-flex>
+          </v-layout>
+        </v-card>
+      </v-layout>
     </v-navigation-drawer>
 
     <v-main>
